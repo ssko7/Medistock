acf51cd9c361203b8cd15cc9f89cbe84d1585827
ssko7 <165413064+ssko7@users.noreply.github.com>
2025-07-07 15:05:39 +0530

    Update settings.py

--- a/settings.py
+++ a/settings.py
@@ -84,10 +84,10 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'postgres',
-        'USER': 'postgres',
+        'USER': 'postgres.qmtikyceszndehspldyv',
         'PASSWORD': 'Saikri@27',  # ⬅️ Replace this
-        'HOST': 'db.qmtikyceszndehspldyv.supabase.co',
-        'PORT': '5432',
+        'HOST': 'aws-0-ap-southeast-1.pooler.supabase.com',
+        'PORT': '6543',
     }
 }
 
